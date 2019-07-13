b73a573fdb05a05a1fbbd04226cbf277739ba9ea
DaniBlaney <daniblaney@gmail.com>
2019-07-13 13:36:12 -0400

    exported compostion page

--- src/components/composition-page/index.js
+++ src/components/composition-page/index.js
@@ -2,3 +2,18 @@ import React from 'react';
 import './style.css';
 import axios from 'axios'
 
+const CreateDemoPage = () => (<>
+  <div className= "noteflight">
+
+
+
+  </div>
+
+</>)
+
+
+
+
+
+
+export default CompostionPage
