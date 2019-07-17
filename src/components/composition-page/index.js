173d6f8d670a8818f3fd05eb7da1e4c00196efc4
skiel <platypony14@gmail.com>
2019-07-16 20:56:08 -0400

    fix comp page to iframe

--- src/components/composition-page/index.js
+++ src/components/composition-page/index.js
@@ -4,28 +4,10 @@ import './style.css';
 // import scipt from '../../../public/static/script'
 
 const CompostionPage = () => (<>
-  <div id="score1">
-
-
-  </div>
-  {/* <script>
-
-    NFClient.init(function(info) {
-
-    })
-    const options = {
-      host: 'www.noteflight.com',
-      width: 800,
-      height: 400,
-      viewParams: {
-        scale: 1.5,
-        role: 'template',
-        app: 'html5'
-      }
-    }
-    const scoreView = new NFClient.ScoreView('score1', 'fcfd6d0bc0770f67cdbe1b8129456521fec090a0', options);
-  </script> */}
-  </>
+  <iframe id="score1" title="score"
+    src="https://www.noteflight.com/embed/fcfd6d0bc0770f67cdbe1b8129456521fec090a0?scale=1.5&role=template&app=html5"
+    width="800" height="400"></iframe>
+</>
 )
 
 
