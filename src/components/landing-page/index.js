5a09f1364e6b019c3b63ada2d2ce31b24ee0a8b1
DaniBlaney <daniblaney@gmail.com>
2019-07-20 12:23:26 -0400

    imported reactbootstrap cards

--- src/components/landing-page/index.js
+++ src/components/landing-page/index.js
@@ -1,82 +1,48 @@
 import React from 'react';
 import './style.css';
+import {Card, CardImg, CardBody, CardText} from 'react-bootstrap';
+
 import NavBarComponent from '../navbar'
 
 
 
 const Saved = () => {
   return (<>
-     <NavBarComponent />
-  <div class="container">
-    <div class="row">
-      <div class="col-md-8">
-        <div class="page-header">
-          <h1>Bach to the Basics</h1>
-        </div>
-        <div class="row">
-          <div class="col-md-4">
-            <img src="images/bach-cool-1.jpg" alt="Cool Bach" class="bach"></img>
-          </div>
-          <div class="col-md-7">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim, felis ut dictum tristique,
-              nibh erat feugiat libero, sit amet fringilla mauris velit in mi. Duis tempus felis vitae
-              felis vulputate pretium. Nullam commodo, est ac auctor ornare, eros nisi fringilla sem,
-              non pulvinar tortor lorem sit amet sem.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4">
-        <div class="card mt-2">
-          <div class="card-body" id="circle-of-fifths">
-            <h5 class="card-title">The Circle of Fifths</h5>
-            <h6 class="card-subtitle mb-2 text-muted"></h6>
-            <img src="images/circle.png" alt="Circle of fifths" width="200"></img>
-            <p class="card-text"></p>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    {/* <div class="row">
-      <div class="col-md-2"></div>
-      <div class="col-md-10">
-        <label class="radio-inline"><input type="radio" name="optradio">A</input></label>
-        <label class="radio-inline"><input type="radio" name="optradio">B</input></label>
-        <label class="radio-inline"><input type="radio" name="optradio">C</input></label>
-        <label class="radio-inline"><input type="radio" name="optradio">D</input></label>
-        <label class="radio-inline"><input type="radio" name="optradio">E</input></label>
-        <label class="radio-inline"><input type="radio" name="optradio">F</input></label>
-        <label class="radio-inline"><input type="radio" name="optradio">G</input></label>
-      </div>
-    </div>
+  <NavBarComponent />
+    <div class="container">
       <div class="row">
-        <div class="col-md-2"></div>
-        <div class="col-md-2">
-          <label class="radio-inline"><input type="radio" name="optradio">#</input></label>
-          <label class="radio-inline"><input type="radio" name="optradio">&#9837</input></label>
+        <div class="col-md-8">
+          <div class="page-header">
+            <h1>Bach to the Basics</h1>
+          </div>
         </div>
-        <div class="col-md-3">
-          <label class="radio-inline"><input type="radio" name="optradio">Major</input></label>
-          <label class="radio-inline"><input type="radio" name="optradio">minor</input></label>
+      </div>
+        <div class="row">
+          <div class="col-5">
+            <Card style={{ width: '20rem' }}>
+              <Card.Img variant="top" src="images/bach-cool-1.jpg" ></Card.Img>
+                <Card.Body>
+                  <Card.Text>
+                   Let's get Bach to the Basics! Here you can learn scales, compose your own songs, and more!
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+          <div class="col-7">
+            <Card style={{ width: '23rem' }}>
+              <Card.Body>
+                <Card.Title>The Circle of Fifths</Card.Title>
+                <Card.Img variant="top" src="images/circle.png" alt="Circle of fifths"></Card.Img>
+                {/* <Card.Link href="#">Card Link</Card.Link> */}
+                {/* <Card.Link href="#">Another Link</Card.Link> */}
+              </Card.Body>
+            </Card>
+          </div>
         </div>
-    </div>
-    <div class="row">
-      <div class="col-md-2"></div>
-        <div class="col-md-10">
-          <label class="radio-inline"><input type="radio" name="optradio">Ionian (major)</input></label>
-          <label class="radio-inline"><input type="radio" name="optradio">Dorian (minor)</input></label>
-          <label class="radio-inline"><input type="radio" name="optradio">Phrygian</input></label>
-          <label class="radio-inline"><input type="radio" name="optradio">Lydian</input></label>
-          <label class="radio-inline"><input type="radio" name="optradio">Mixolydian (dominant)</input></label>
-          <label class="radio-inline"><input type="radio" name="optradio">Aeolian</input></label>
-          <label class="radio-inline"><input type="radio" name="optradio">Locrian</input></label>
-        </div>
-    </div> */}
-  </div>
+      </div>
+</>
 
-  </>)
-  }
+  )
+}
 
   export default Saved
