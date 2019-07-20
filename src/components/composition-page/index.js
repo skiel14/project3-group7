500fc7b21f9326b607063b3924bf06eb2da4e559
DaniBlaney <daniblaney@gmail.com>
2019-07-20 13:22:15 -0400

    adjusted compose page

--- src/components/composition-page/index.js
+++ src/components/composition-page/index.js
@@ -7,9 +7,13 @@ import NavBarComponent from '../navbar'
 
 const CompostionPage = () => (<>
  <NavBarComponent />
-  <iframe id="score1" title="score"
-    src="https://www.noteflight.com/embed/fcfd6d0bc0770f67cdbe1b8129456521fec090a0?scale=1.5&role=template&app=html5"
-    width="800" height="400"></iframe>
+  <div className="flight">
+    <iframe id="score1" title="score"
+      src="https://www.noteflight.com/embed/fcfd6d0bc0770f67cdbe1b8129456521fec090a0?scale=1.5&role=template&app=html5"
+      width="800" height="400">
+    </iframe>
+</div>
+
   </>
 )
 
