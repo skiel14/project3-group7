7a881543cc8c9cf1a15a6acad077fede0aa8819f
DaniBlaney <daniblaney@gmail.com>
2019-07-27 10:50:04 -0400

    converted landing page to react bootstrap

--- src/components/landing-page/index.js
+++ src/components/landing-page/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './style.css';
-import {Card, CardImg, CardBody, CardText, Col} from 'react-bootstrap';
+import {Card, CardImg, CardBody, CardText, Col, Row, Container} from 'react-bootstrap';
 import NavBarComponent from '../navbar'
 import RadioBtns from '../radio-btns'
 
@@ -9,41 +9,41 @@ import RadioBtns from '../radio-btns'
 const Saved = () => {
   return (<>
   <NavBarComponent />
-    <div className="container">
-      <div className="row">
-        <div className="col-md-12">
+    <Container>
+      <Row>
+        <Col className="md-12">
           <div className="page-header">
             <h1>Bach to the Basics</h1>
           </div>
-        </div>
-      </div>
-        <div className="row">
-          <div className="Col-4">
+        </Col>
+      </Row>
+        <Row>
+          <Col className="md-4">
             <Card style={{ width: '23rem' }}>
               <Card.Img variant="top" src="images/bach-cool-1.jpg" ></Card.Img>
-              </Card>
-              </div>
-              <div className="col-8">
+            </Card>
+          </Col>
+            <Col className="md-8">
               <Card>
                 <Card.Body>
                   <Card.Text>
-                    Let's get Bach to the Basics! What are scales? Why should people learn them? What is the point? Where should I start? Here you can learn scales, compose your own songs, and more! Learn the "Fun" damentals on our Practice page!! Regular piano playing offers different physical and physiological advantages to players. It sharpens fine motor skills, improves dexterity and hand-eye coordination. Music has also been shown to reduce heart and respiratory rates, cardiac complications, and to lower blood pressure and increase immune response.
+                    Let's get Bach to the Basics! What are scales? Why should people learn them? Where should I start? Here you can learn scales, compose your own songs, and more! Learn the "Fun" damentals on our Practice page!! Regular piano playing offers different physical and physiological advantages to players. It sharpens fine motor skills, improves dexterity and hand-eye coordination.
                   </Card.Text>
                 </Card.Body>
               </Card>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-12">
+          </Col>
+        </Row>
+        <Row>
+          <Col className="col-12">
             <Card style={{ width: '23rem' }}>
               <Card.Body>
                 <Card.Title>Cats jammin out</Card.Title>
                 <Card.Img variant="top" src="images/cat.gif" alt="Cat"></Card.Img>
               </Card.Body>
             </Card>
-          </div>
-        </div>
-      </div>
+          </Col>
+        </Row>
+      </Container>
 </>
 
   )
